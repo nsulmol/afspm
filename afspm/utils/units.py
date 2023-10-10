a9a1b6ef950fb460f7740cf75da07982a3de87c1
Nick Sullivan <nsulmol@gmail.com>
2023-10-10 11:31:30 -0400

    feat: add units conversion method using pint
    Forgot toa dd this in a previous CL. This module has a method for
    converting between units, using pint.

---
+++ afspm/utils/units.py
@@ -0,0 +1,37 @@
+"""Simple method using pint to convert"""
+
+import logging
+from typing import Any
+
+import pint
+
+
+logger = logging.getLogger(__name__)
+
+
+# Note: pint's unit registry will exist *HERE*. If you need it, import it in
+# your module.
+#
+ureg = pint.UnitRegistry()
+Q_ = ureg.Quantity
+
+
+# TODO: Add test for me!!
+def convert(val: Any, unit: str, desired_unit: str) -> Any:
+    """Uses pint to convert a value from one unit to another.
+
+    Args:
+        val: input value, of a type that pint supports.
+        unit: str representation of your current unit.
+        desired_unit: str representation of your desired unit.
+
+    Returns:
+        val converted into desired unit.
+
+    Raises:
+        DimensionalityError if you try to perform an impossible conversion (or
+        one it does not know how to do).
+        UndefinedUnitError if either unit or desired_unit are undefined.
+    """
+    quantity = val * ureg(unit)
+    return quantity.to(desired_unit)
