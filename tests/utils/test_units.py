d51fd541926e9357cd4e10f75ff985f97ad08b2e
Nick Sullivan <nsulmol@gmail.com>
2023-10-12 18:44:15 -0400

    feat: add units test

---
+++ tests/utils/test_units.py
@@ -0,0 +1,45 @@
+"""Validate methods in units work."""
+
+import pytest
+import pint
+from numpy.testing import assert_almost_equal
+
+import afspm.utils.units as units
+
+
+@pytest.fixture
+def val_nm():
+    return 10.55
+
+
+@pytest.fixture
+def units_nm():
+    return 'nm'
+
+
+@pytest.fixture
+def val_ang():
+    return 105.5
+
+
+@pytest.fixture
+def units_ang():
+    return 'angstrom'
+
+
+@pytest.fixture
+def units_fake():
+    return 'banana'
+
+
+def test_convert(val_nm, units_nm, val_ang, units_ang):
+    res_ang = units.convert(val_nm, units_nm, units_ang)
+    assert_almost_equal(res_ang, val_ang)
+
+    res_nm = units.convert(val_ang, units_ang, units_nm)
+    assert_almost_equal(res_nm, val_nm)
+
+
+def test_break(val_nm, units_nm, units_fake):
+    with pytest.raises(pint.UndefinedUnitError):
+        units.convert(val_nm, units_nm, units_fake)
