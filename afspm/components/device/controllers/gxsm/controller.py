7ea65681d426fe22b93e5ec3f7ec629bba83a818
Nick Sullivan <nsulmol@gmail.com>
2023-10-13 10:39:42 -0400

    feat: expose gxsmread params to controller
    Needed for us to be able to use the gxsm controller properly...

--- afspm/components/device/controllers/gxsm/controller.py
+++ afspm/components/device/controllers/gxsm/controller.py
@@ -27,7 +27,6 @@ class GxsmController(DeviceController):
     """
 
     GET_FAILURE = '\x04'
-    GXSM_PHYS_UNITS = 'angstrom'  # Unit the gxsm data is in #TODO Confirm!!!
     TL_X = 'OffsetX'
     TL_Y = 'OffsetY'
     SZ_X = 'RangeX'
@@ -44,11 +43,16 @@ class GxsmController(DeviceController):
     # NOTE: I'm thinking each side is responsible for converting the data to the
     # units they want
     def __init__(self,
-                 channels_config_path: str = "./channels_config.toml",
-                 **kwargs):
-        #, default_physical_units: str = 'nanometer'):
-        self.channels_config_path = channels_config_path
-        #self.sent_physical_units = default_physical_units
+                 read_channels_config_path: str = "./channels_config.toml",
+                 read_use_physical_units: bool = "True",
+                 read_allow_convert_from_metadata: bool = "False",
+                 read_simplify_metadata: bool = "True",
+                 gxsm_physical_units: str = 'angstrom', **kwargs):
+        self.read_channels_config_path = read_channels_config_path
+        self.read_use_physical_units = read_use_physical_units
+        self.read_allow_convert_from_metadata = read_allow_convert_from_metadata
+        self.read_simplify_metadata = read_simplify_metadata
+        self.gxsm_physical_units = gxsm_physical_units  # TODO: read from gxsm
         self.last_scan_fname = ''
         self.last_scan_state = scan_pb2.ScanState.SS_UNDEFINED
 
@@ -76,9 +80,15 @@ class GxsmController(DeviceController):
                       scan_params.spatial.units,
                       scan_params.spatial.units,
                       scan_params.spatial.units,
-                      None, None]
+                      scan_params.data.units,
+                      scan_params.data.units]
+        gxsm_units = [self.gxsm_physical_units,
+                      self.gxsm_physical_units,
+                      self.gxsm_physical_units,
+                      self.gxsm_physical_units,
+                      None, None]  # TODO: Update with data units
 
-        if self._gxsm_set_list(attrs, vals, attr_units):
+        if self._gxsm_set_list(attrs, vals, attr_units, gxsm_units):
             return control_pb2.ControlResponse.REP_SUCCESS
         return control_pb2.ControlResponse.REP_ATTRIB_ERROR
 
@@ -92,7 +102,8 @@ class GxsmController(DeviceController):
         return state
 
     def poll_scan_params(self) -> scan_pb2.ScanParameters2d:
-        return self._get_current_scan_params()
+        # TODO: Update to provide data units
+        return self._get_current_scan_params(self.gxsm_phys_units)
 
     def poll_scans(self) -> [scan_pb2.Scan2d]:
         channel_idx = 0
@@ -116,7 +127,10 @@ class GxsmController(DeviceController):
             scans = []
             for fname in fnames:
                 ts = get_file_modification_datetime(fname)
-                ds = read.opendataset(fname, self.channels_config_path)
+                ds = read.open_dataset(fname, self.read_channels_config_path,
+                                       self.read_use_physical_units,
+                                       self.read_allow_convert_from_metadata,
+                                       self.read_simplify_metadata)
                 scan = conv.convert_xarray_to_scan_pb2(
                     ds[list(ds.data_vars)[0]])
                 scan.timestamp.FromDatetime(ts)
@@ -128,46 +142,47 @@ class GxsmController(DeviceController):
 
     # TODO: Should this just be poll_scan_params???
     @staticmethod
-    def _get_current_scan_params():
+    def _get_current_scan_params(gxsm_phys_units: str,
+                                 gxsm_data_units: str = ''):
+        """Poll gxsm for current scan parameters and fill object."""
         scan_params = scan_pb2.ScanParameters2d()
         # TODO: how to handle units??  For now, just send out how they are received?
         scan_params.spatial.roi.top_left.x = gxsm.get(GxsmController.TL_X)
         scan_params.spatial.roi.top_left.y = gxsm.get(GxsmController.TL_Y)
         scan_params.spatial.roi.size.x = gxsm.get(GxsmController.SZ_X)
         scan_params.spatial.roi.size.y = gxsm.get(GxsmController.SZ_Y)
-        scan_params.spatial.units = GxsmController.GXSM_PHYS_UNITS
+        scan_params.spatial.units = gxsm_phys_units
 
         # Note: all gxsm attributes returned as float, must convert to int
         scan_params.data.shape.x = int(gxsm.get(GxsmController.RES_X))
         scan_params.data.shape.y = int(gxsm.get(GxsmController.RES_Y))
-        #scan_params.data.units = ???  # TODO: how to read units?
+        scan_params.data.units = gxsm_data_units
 
         return scan_params
 
     @staticmethod
-    def _gxsm_set(attr: str, val: Any, curr_units: str = None):
+    def _gxsm_set(attr: str, val: Any, curr_units: str = None,
+                  gxsm_units: str = None):
         """Convert a value to gxsm units and set it."""
-        # If curr_units is None, we don't convert
-        if curr_units:
+        if curr_units and gxsm_units and curr_units != gxsm_units:
             val = units.convert(val, curr_units,
-                                GxsmController.GXSM_PHYS_UNITS)
+                                gxsm_units)
         gxsm.set(attr, str(val))
 
     @staticmethod
     def _gxsm_set_list(attrs: list[str], vals: list[Any],
-                       curr_units: list[str | None]) -> bool:
+                       curr_units: list[str | None],
+                       gxsm_units: list[str | None]) -> bool:
         """Convert a list of values to gxsm units and set them."""
         converted_vals = []
-        for val, curr_unit in zip(vals, curr_units):
-            if curr_unit:
+        for val, curr_unit, gxsm_unit in zip(vals, curr_units, gxsm_units):
+            if curr_unit and gxsm_unit and curr_unit != gxsm_unit:
                 try:
                     converted_vals.append(
-                        units.convert(val, curr_unit,
-                                      GxsmController.GXSM_PHYS_UNITS))
+                        units.convert(val, curr_unit, gxsm_unit))
                 except UndefinedUnitError:
                     logger.error("Unable to convert %s from %s to %s.",
-                                 val, curr_unit,
-                                 GxsmController.GXSM_PHYS_UNITS)
+                                 val, curr_unit, gxsm_unit)
                     return False
             else:
                 converted_vals.append(val)
