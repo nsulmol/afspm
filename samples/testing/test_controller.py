8a73e788f3a58e49acec0bc027ec5ee6949b9847
Nick Sullivan <nsulmol@gmail.com>
2023-12-06 19:32:47 -0500

    [UNTESTED] feat: update test_controller.py
    Doing 2 things here:
    - Updating the test suite to be based off of the config.toml (i.e. avoid
    duplication), reading a config.toml to set fixtures before running.
    - Adding a ZCtrlParams test.
    
    Note that this *has not been tested*! I will probably need to update
    this tomorrow, after testing with gxsm.

--- samples/testing/test_controller.py
+++ samples/testing/test_controller.py
@@ -1,17 +1,28 @@
 """This test suite exists to validate device controller functionality.
 
+In order to run it, you must:
+1. Start up the afspmcon, via a call like the following:
+    spawn config.toml --components_to_spawn ['afspmcon']
+2. Start up the device controller you wish to test. This startup will be
+implementation-specific (look at the readme for your controller).
+3. Run these tests:
+    pytest $PATH_TO_TEST/test_controller.py --config_path $CONFIG_FILE_PATH
+Note the default config path is './config.toml'.
+
+Your config file contains the parameters for our tests. See sample_config.toml
+for guidance.
+
 Ideally, you should set your controller's scan parameters to scan quickly
 before running these tests. For example, increase your scan speed and
 decrease your ROI size to pass the test more quickly.
-
-Note that the scan_wait_ms is only 180s!
 """
 
 import logging
 import copy
+import time
+import tomli
 import pytest
 import zmq
-import time
 
 from google.protobuf.message import Message
 
@@ -24,12 +35,28 @@ from afspm.components.afspm.controller import AfspmController
 
 from afspm.io.protos.generated import scan_pb2
 from afspm.io.protos.generated import control_pb2
+from afspm.io.protos.generated import feedback_pb2
 
 
 logger = logging.getLogger(__name__)
 
 
+def pytest_addoption(parser):
+    parser.addoption("config_path", action="store", default="./config.toml",
+                     help="Path to config file, from which to load params.")
+
+
 # -------------------- Fixtures -------------------- #
+@pytest.fixture(scope="session")
+def config_path(request):
+    return request.config.get_option("--config_path")
+
+
+@pytest.fixture(scope="session")
+def config_dict(config_path):
+    return tomli.load(config_path)
+
+
 # --- General / Urls --- #
 @pytest.fixture
 def ctx():
@@ -43,28 +70,32 @@ def component_name():
 
 @pytest.fixture(scope="module")
 def timeout_ms():
-    return 5000
+    return config_dict['timeout_ms']
 
 
 @pytest.fixture(scope="module")
 def scan_wait_ms():
-    return 180000
+    return config_dict['scan_wait_ms']
 
 
 @pytest.fixture(scope="module")
 def psc_url():
-    return "tcp://127.0.0.1:7778"
+    return config_dict['psc_url']
 
 
 @pytest.fixture(scope="module")
-def server_url():
-    return "tcp://127.0.0.1:7777"
+def router_url():
+    return config_dict['router_url']
+
+@pytest.fixture(scope="module")
+def control_mode():
+    return control_pb2.ControlMode.CM_AUTOMATED
 
 
 @pytest.fixture(scope="module")
-def default_control_state():
+def default_control_state(control_mode):
     cs = control_pb2.ControlState()
-    cs.control_mode = control_pb2.ControlMode.CM_AUTOMATED
+    cs.control_mode = control_mode
     return cs
 
 
@@ -83,6 +114,10 @@ def topics_scan_params():
 @pytest.fixture(scope="module")
 def topics_scan_state():
     return [cl.CacheLogic.get_envelope_for_proto(scan_pb2.ScanStateMsg())]
+
+@pytest.fixture(scope="module")
+def topics_zctrl():
+    return [cl.CacheLogic.get_envelope_for_proto(feedback_pb2.ZCtrlParameters)]
 
 
 # --- I/O Classes (Subscribers, Clients) --- #
@@ -107,9 +142,15 @@ def sub_scan_params(ctx, topics_scan_params, timeout_ms, psc_url):
                       poll_timeout_ms=timeout_ms)
 
 
+def sub_zctrl(ctx, topics_zctrl, timeout_ms, psc_url):
+    return Subscriber(psc_url,
+                      topics_to_sub=topics_zctrl,
+                      poll_timeout_ms=timeout_ms)
+
+
 @pytest.fixture
-def client(ctx, component_name, server_url):
-    return ControlClient(server_url, ctx, component_name)
+def client(ctx, component_name, router_url):
+    return ControlClient(router_url, ctx, component_name)
 
 
 # -------------------- Helper Methods -------------------- #
@@ -134,13 +175,22 @@ def stop_client(client: ControlClient):
     del client  # Explicitly kill to avoid zmq weirdness.
     time.sleep(1)
 
+def startup_grab_control(client: ControlClient,
+                         control_mode: control_pb2.ControlMode.CM_AUTOMATED):
+    """Request and get control of the device controller."""
+    rep = client.request_control(control_mode)
+    assert rep == control_pb2.ControlResponse.REP_SUCCESS
+
 
 # -------------------- Tests -------------------- #
-def test_cancel_scan(client, default_control_state, component_name,
-                     sub_scan, sub_scan_state, timeout_ms):
+def test_cancel_scan(client, default_control_state,
+                     sub_scan, sub_scan_state, timeout_ms,
+                     control_mode):
     logger.info("Validate we can start and cancel a scan.")
     logger.info("First, validate we *do not* have an initial scan (in the "
                 "cache), and *do* have an initial scan state (SS_FREE).")
+    startup_grab_control(client, control_mode)
+
     scan_state_msg = scan_pb2.ScanStateMsg(
         scan_state=scan_pb2.ScanState.SS_FREE)
 
@@ -183,11 +233,13 @@ def test_cancel_scan(client, default_control_state, component_name,
     stop_client(client)
 
 
-def test_scan_params(client, default_control_state, component_name,
-                     sub_scan_params):
+def test_scan_params(client, default_control_state,
+                     sub_scan_params, control_mode):
     logger.info("Validate we can set scan parameters.")
     logger.info("First, validate we have initial scan params (from the "
                 "cache).")
+    startup_grab_control(client, control_mode)
+
     __, initial_params = sub_scan_params.poll_and_store()
     assert initial_params
 
@@ -218,11 +270,13 @@ def test_scan_params(client, default_control_state, component_name,
     stop_client(client)
 
 
-def test_run_scan(client, default_control_state, component_name,
-                  sub_scan, sub_scan_state, timeout_ms):
+def test_run_scan(client, default_control_state,
+                  sub_scan, sub_scan_state, timeout_ms,
+                  control_mode):
     logger.info("Validate we can start a scan, and receive one on finish.")
     logger.info("First, validate we *do not* have an initial scan (in the "
                 "cache), and *do* have an initial scan state (SS_FREE).")
+    startup_grab_control(client, control_mode)
 
     scan_state_msg = scan_pb2.ScanStateMsg(
         scan_state=scan_pb2.ScanState.SS_FREE)
@@ -248,5 +302,38 @@ def test_run_scan(client, default_control_state, component_name,
     assert sub_scan.poll_and_store()
     scan_state_msg.scan_state = scan_pb2.ScanState.SS_FREE
     assert_sub_received_proto(sub_scan_state, scan_state_msg)
+
+    stop_client(client)
+
+
+def test_handle_zctrl(client, default_control_state,
+                      sub_zctrl, timeout_ms,
+                      control_mode):
+    logger.info("Validate we recieve and can set ZCtrlParams.")
+
+    startup_grab_control(client, control_mode)
+
+    logger.info("First, ensure we receive initial ZCtrlParams.")
+    __, initial_params = sub_zctrl.poll_and_store()
+    assert initial_params
+
+    modified_params = copy.deepcopy(initial_params)
+    modified_params.proportionalGain *= 1.1
+    modified_params.integralGain *= 1.1
+
+    logger.info("Next, set new ZCtrlParams. We expect a success.")
+    rep = client.set_zctrl_params(modified_params)
+    assert rep == control_pb2.ControlResponse.REP_SUCCESS
+
+    logger.info("Next, validate that our subscriber receives these new "
+                "params.")
+    __, last_params = sub_zctrl.poll_and_store()
+    assert last_params == modified_params
+
+    logger.info("Now, return to our initial parameters.")
+    rep = client.set_zctrl_params(initial_params)
+    assert rep == control_pb2.ControlResponse.REP_SUCCESS
+    __, last_params = sub_zctrl.poll_and_store()
+    assert last_params == initial_params
 
     stop_client(client)
