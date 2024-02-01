6ae7a22fa1e1fc3a5105c6b76cb8aa4ccdfdf7f5
Nick Sullivan <nsulmol@gmail.com>
2024-02-01 10:50:38 -0500

    refactor: update utils/log.py slightly
    Mostly commenting/naming/default arguments changes for the methods in
    this module.

--- afspm/utils/log.py
+++ afspm/utils/log.py
@@ -1,7 +1,4 @@
-"""Allows adding new log types to logger package.
-
-Taken directly from: https://stackoerflow.com/a/35804945.
-"""
+"""Logging methods/helpers."""
 
 import logging
 from colorlog import ColoredFormatter
@@ -12,7 +9,7 @@ from types import MappingProxyType  # Immutable dict
 LOGGER_ROOT = 'afspm'
 TRACE_LOG_LEVEL = logging.DEBUG - 5
 
-LOG_LEVEL_STR_TO_VAL = MappingProxyType({
+LOG_LEVEL_STR_TO_INT = MappingProxyType({
     'NOTSET': logging.NOTSET,
     'TRACE': TRACE_LOG_LEVEL,
     'DEBUG': logging.DEBUG,
@@ -38,6 +35,8 @@ def addLoggingLevel(levelName, levelNum, methodName=None):
     To avoid accidental clobberings of existing attributes, this method will
     raise an `AttributeError` if the level name is already an attribute of the
     `logging` module or if the method name is already present
+
+    Taken directly from: https://stackoerflow.com/a/35804945.
 
     Example
     -------
@@ -76,22 +75,23 @@ def addLoggingLevel(levelName, levelNum, methodName=None):
     setattr(logging, methodName, logToRoot)
 
 
-def set_up_logging(log_file: str, log_to_stdout: bool, log_level: str):
+def set_up_logging(log_file: str = None, log_to_stdout: bool = True,
+                   log_level: str | int = logging.INFO):
     """Set up logging logic.
 
     Args:
-        log_file: a file path to save the process log. Default is 'log.txt'.
-            To not log to file, set to None.
+        log_file: a file path to save the process log. Default is None.
         log_to_std_out: whether or not we print to std out as well. Default is
             True.
-        log_level: the log level to use. Default is INFO.
+        log_level: the log level to use, as a string or int. Default is INFO.
     """
     root = logging.getLogger(LOGGER_ROOT)
 
     if root.hasHandlers():  # Delete existing handlers before adding ours
         root.handlers.clear()
 
-    log_level = LOG_LEVEL_STR_TO_VAL[log_level.upper()]
+    if isinstance(log_level, str):
+        log_level = LOG_LEVEL_STR_TO_INT[log_level.upper()]
     root.setLevel(log_level)
     formatter = logging.Formatter(
         '%(asctime)s | %(name)s | '
