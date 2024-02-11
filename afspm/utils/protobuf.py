7f5e94c63110b85ab2272ba0e4f7a3ba72c1cc60
Nick Sullivan <nsulmol@gmail.com>
2024-02-11 18:54:22 -0500

    feat: add rel_tol equals check for protobuf msgs
    This is important for floating point comparisons, and to account for
    varying acceptable ranges between different SPMs.

---
+++ afspm/utils/protobuf.py
@@ -0,0 +1,33 @@
+"""Holds protobuf helper methods."""
+
+import math
+
+from google.protobuf.message import Message
+
+
+def check_equal(msg1: Message, msg2: Message,
+                rel_tol: float = 1e-09) -> bool:
+    """Check if two messages are equal.
+
+    Args:
+        msg1: first message to compare.
+        msg2: second message to compare.
+        rel_tol: optional relative tolerance, for float comparisons.
+
+    Returns:
+        True if the two messages are equal.
+    """
+    for field in msg1.DESCRIPTOR.fields:
+        if ((msg1.HasField(field.name) and not msg2.HasField(field.name)) or
+                (msg2.HasField(field.name) and not msg1.HasField(field.name))):
+            return False
+
+        val1 = getattr(msg1, field.name)
+        val2 = getattr(msg2, field.name)
+        is_float = isinstance(val1, float)
+
+        if is_float and not math.isclose(val1, val2, rel_tol):
+            return False
+        if not is_float and val1 != val2:  # Value comparison
+            return False
+    return True
