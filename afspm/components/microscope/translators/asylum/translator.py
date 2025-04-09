b583eb6eb919699504d58b8dd1b6dd4ece91d485
Nick Sullivan <nsulmol@gmail.com>
2025-04-09 16:28:57 -0400

    [fix] fix in poll_scope_state
    You had forgotten to return SS_FREE if none of the bit checks are valid...

--- afspm/components/microscope/translators/asylum/translator.py
+++ afspm/components/microscope/translators/asylum/translator.py
@@ -230,6 +230,7 @@ class AsylumTranslator(ConfigTranslator):
             return scan_pb2.ScopeState.SS_SPEC
         elif params.ScopeState.MOVING.value & val:
             return scan_pb2.ScopeState.SS_MOVING
+        return scan_pb2.SS_FREE
 
     def _get_latest_file(self, prefix: str) -> str | None:
         val = self.param_handler.get_param(params.AsylumParam.IMG_PATH.name)
