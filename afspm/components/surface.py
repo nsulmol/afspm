f8d8f8f7e31b8f3a31e9545d838bcfc2d4efbb69
Nick Sullivan <nsulmol@gmail.com>
2025-04-18 18:06:54 -0400

    [feat] first pass of surface estimation
    This is a first pass on surface estimation, using multiple sklearn
    LinearRegressors. I say first pass because the tests are very basic, and
    I have not really fiddled with the various regressor options to figure
    out what paremeters are best for each.
    
    TODO: Investigate best parameters and parameter kwargs.

---
+++ afspm/components/surface.py
@@ -0,0 +1,254 @@
+"""Holds logic for fitting surfaces, subtracting them, setting feedback slope.
+
+REMINDER: matrices are (row, col), so (y, x). This is why we do yx coords
+throughout.
+
+How to use:
+    import afspm.utils.array_converters as ac
+    import afspm.components.surface as surface
+
+    model = surface.create_surface_model(...)
+
+    # Fit data to model
+    da = ac.convert_scan_pb2_to_xarray(scan)
+    (X, y) = convert_xarray_to_Xy(da)
+    model.fit()
+
+    # Create DataArray of surface
+    surf_y = model.predict(X)
+    surf_y = surface_y.reshape(da.shape)
+    surf_da = da.copy(data=surf_y)
+
+    # Get true deviation scan (removing background)
+    true_data = da - surf_da
+"""
+
+from enum import Enum
+import logging
+
+import numpy as np
+import xarray as xr
+from matplotlib.axes import Axes
+from matplotlib.colors import Colormap
+from matplotlib import pyplot as plt
+
+from sklearn.base import BaseEstimator
+from sklearn.linear_model import (
+    HuberRegressor,
+    LinearRegression,
+    RANSACRegressor,
+    TheilSenRegressor,
+    QuantileRegressor
+)
+
+from sklearn.pipeline import make_pipeline
+from sklearn.preprocessing import PolynomialFeatures
+
+from afspm.io.protos.generated import scan_pb2
+
+
+logger = logging.getLogger(__name__)
+
+
+class FittingMethod(str, Enum):
+    """Fitting method used to fit the surface.
+
+    If curious about the various methods, please consult sklearn and
+    their Supervised Learning - Linear Regression section (1.1).
+    """
+
+    LEAST_SQUARES = 'LeastSquares'
+    THEIL_SEN = 'Theil-Sen'
+    RANSAC = 'RANSAC'
+    HUBER = 'Huber'
+    QUANTILE = 'Quantile'  # Defaults to median regressor (i.e. MAE)
+
+
+def _create_estimator(method: FittingMethod = FittingMethod.LEAST_SQUARES,
+                      **kwargs) -> BaseEstimator:
+    """Create simple estimator based on provided method and kwargs."""
+    match method:
+        case FittingMethod.LEAST_SQUARES:
+            return LinearRegression(**kwargs)
+        case FittingMethod.THEIL_SEN:
+            return TheilSenRegressor(**kwargs)
+        case FittingMethod.RANSAC:
+            return RANSACRegressor(**kwargs)
+        case FittingMethod.HUBER:
+            return HuberRegressor(**kwargs)
+        case FittingMethod.QUANTILE:
+            return QuantileRegressor(**kwargs)
+
+
+def create_surface_model(polynomial_degree: int = 1,
+                         method: FittingMethod = FittingMethod.LEAST_SQUARES,
+                         **kwargs) -> BaseEstimator:
+    """Create a linear model for fitting a sample surface.
+
+    The fitting can later be used to, for example, subtract the background (in
+    order to get better texture information, i.e. variation relative to overall
+    surface).
+
+    Args:
+        polynomial_degree: the polynomial degree used to model the surface. A
+            value of 1 would correspond to a linear fit, 2 a quadratic fit, 3
+            a 3rd order polynomial, etc. Defaults to a linear fit (i.e. 1).
+        method: the fitting method used to estimate the surface. Defaults to
+            a least-squares fit.
+        **kwargs: any additional arguments needed to instantiate the estimator.
+
+    Returns:
+        The BaseEstimator to be used for fitting.
+    """
+    estimator = _create_estimator(method, **kwargs)
+    return make_pipeline(PolynomialFeatures(polynomial_degree), estimator)
+
+
+def convert_scan_to_Xy(scan: scan_pb2.Scan2d) -> (np.array, np.array):
+    """Extract (X,y) form of provided 2D scan.
+
+    A scan consists of a 2D region, where each position in the region contains a
+    value. In order to fit a model to this, we need to convert it into a set of
+    (X, y) pairs, where X is a vector of input values (in this case, the
+    2D yx positions), and y is the scalar output (in this case, the value
+    at each yx position). This method accomplishes that.
+
+    NOTE: this method considers the raw data, i.e. the yx positions are
+    simply indices, and there are no units. For a unit-ed approach,
+    use convert_xarray_to_Xy().
+
+    Args:
+        scan: Scan2d containing the yx positions and y/intensity values.
+
+    Returns:
+        (X, y) tuple containing:
+        - X: np.array of shape (N*M, 2), where the input scan is of size
+            N*M. These are the 2D yx positions of the data points.
+        - y: np.array of shape (N*M, 1), where the input scan is of size
+            N*M. These are the intensity values at each position.
+    """
+    indices = np.indices((scan.params.data.shape.y, scan.params.data.shape.x))
+    X = np.column_stack((indices[0].ravel(), indices[1].ravel()))
+    y = np.array(scan.values, dtype=np.float64)
+    return (X, y)
+
+
+def convert_xarray_to_Xy(da: xr.DataArray) -> (np.array, np.array):
+    """Extract (X,y) form of provided xarray DataArray.
+
+    A scan consists of a 2D region, where each position in the region contains a
+    value. In order to fit a model to this, we need to convert it into a set of
+    (X, y) pairs, where X is a vector of input values (in this case, the
+    2D yx positions), and y is the scalar output (in this case, the value
+    at each yx position). This method accomplishes that.
+
+    NOTE: This method specifically assumes:
+    - x and y are labeled 'x' and 'y' in the DataArray.
+    - the 'x' and 'y' coordinates are logical coordinates, i.e. their
+    sizes match the size of the associated dimension.
+    It will reliably work if used in conjunction with array_converters'
+    convert_scan_pb2_to_xarray().
+
+    Args:
+        da: DataArray containing the yx positions and y/intensity values.
+
+    Returns:
+        (X, y) tuple containing:
+        X: np.array of shape (N*M, 2), where the input scan is of size
+            N*M. These are the 2D yx positions of the data points.
+        y: np.array of shape (N*M, 1), where the input scan is of size
+            N*M. These are the intensity values at each position.
+    """
+    assert len(da.y.values) == da.shape[0]
+    assert len(da.x.values) == da.shape[1]
+
+    # Reminder: da.y and da.x are 1D arrays containing the points of each
+    # dimension. To convert to X, we need to have the 2D XY or YX pairs.
+    coords = [[y, x] for y in da.y.values for x in da.x.values]
+
+    X = np.array(coords)
+    y = da.values.ravel()
+    return (X, y)
+
+
+def convert_Xy_to_xarray(X: np.array, y: np.array) -> xr.DataArray:
+    """Create xarray DataArray from provided X and y values.
+
+    NOTE: Assumes box/rectangle/raster!
+
+    Args:
+        X: np.array of shape (N*M, 2), where the input scan is of size
+            N*M. These are the 2D yx positions of the data points.
+        y: np.array of shape (N*M, 1), where the input scan is of size
+            N*M. These are the intensity values at each position.
+    Returns:
+        DataArray
+    """
+    X0 = np.unique(X[:, 0])
+    X1 = np.unique(X[:, 1])
+
+    assert len(X0) * len(X1) == X.shape[0]
+
+    y = np.reshape(y, (len(X0), len(X1)))
+    return xr.DataArray(data=y, dims=['y', 'x'],
+                        coords={'y': X0, 'x': X1})
+
+
+def update_xarray_y(da: xr.DataArray, y: np.array) -> xr.DataArray:
+    """Update da with new 'y' data (intensity values).
+
+    Args:
+        da: DataArray containing original yx positions and y/intensity values.
+        y: np.array of shape (N*M, 1), where the input scan is of size
+            N*M. These are the intensity values at each position.
+    Returns:
+        DataArray with y/intensity values updated.
+    """
+    return da.copy(data=y)
+
+
+def fit_surface(da: xr.DataArray, model: BaseEstimator
+                ) -> (xr.DataArray, float):
+    """Fit a surface model to data.
+
+    Args:
+        da: DataArray of a microscope scan.
+        model: BaseEstimator used to fit surface.
+
+    Returns:
+        (xr.DataArray, float) tuple containing:
+        - A copy of the DataArray, with values representing the surface.
+        - Score value of fitting.
+    """
+    # Fit data to model
+    (X, y) = convert_xarray_to_Xy(da)
+    model.fit(X, y)
+    score = model.score(X, y)
+
+    # Create DataArray of surface
+    surf_y = model.predict(X)
+    surf_y = surf_y.reshape(da.shape)
+    surf_da = da.copy(data=surf_y)
+    return surf_da, score
+
+
+def visualize(original_da: xr.DataArray, surface_da: xr.DataArray,
+              ax: Axes | None = None,
+              cmap: Colormap | str | None = None):
+    """Visualize the estimated surface relative to the original data.
+
+    Args:
+        original_da: DataArray of a microscope scan.
+        surface_da: DataArray of the estimated surface.
+        ax: Axes on which to plot. By default, use current axes.
+        cmap: Mapping from data values to color space. Default is None,
+            which uses an internal plt default.
+    """
+    if ax is None:
+        fig = plt.figure()
+        ax = fig.add_subplot(projection='3d')
+
+    surface_da.plot.surface(ax=ax, cmap=cmap)
+
+    X, y = convert_xarray_to_Xy(original_da)
+    ax.scatter(X[:, 1], X[:, 0], y, color='orange')
---
+++ tests/components/test_surface.py
@@ -0,0 +1,174 @@
+"""Test surface estimation logic."""
+# TODO: Finish testing me!
+
+import logging
+import pytest
+from typing import Callable
+
+import numpy as np
+from sklearn.preprocessing import PolynomialFeatures
+
+from afspm.components import surface
+
+from matplotlib import pyplot as plt
+
+
+logger = logging.getLogger(__name__)
+
+
+# Polynomial feature shape is [1, a, b, a^2, ab, b^2, ...]
+@pytest.fixture
+def plane_surface_coeffs():
+    return [2, 0.2, 0.1]
+
+
+@pytest.fixture
+def poly_2nd_order_surface_coeffs():
+    return [2, 0.2, 0.1, -0.05, 0, 0]
+
+
+@pytest.fixture
+def x_dim():
+    return np.arange(0, 3, 0.1)
+
+
+@pytest.fixture
+def y_dim():
+    return np.arange(0, 3, 0.1)
+
+
+@pytest.fixture
+def texture_kwargs():
+    return {'width': 0.2, 'scale': 0.2, 'ax': 0}
+
+
+@pytest.fixture
+def noise_mean():
+    return 0
+
+
+@pytest.fixture
+def noise_sigma():
+    return 0.05
+
+
+@pytest.fixture
+def min_mse_plane():
+    return 0.013  # TODO: investigate further, you didn't really test this
+
+
+@pytest.fixture
+def min_mse_poly():
+    return 0.025   # TODO: investigate further, you didn't really test this
+
+
+def step_train(yx: np.array, width: float = 1.0,
+               scale: float = 1.0, ax: int = 0) -> float:
+    """Outputs y value of a 'step train' function for position X.
+
+    Args:
+        yx: (y, x) position for which we output the y value.
+        width: step width. Default is 1.0.
+        scale: scale of y value. Default is 1.0.
+        ax: axis upon which the step train happens. Default is 0 (y axis).
+    """
+    return int((yx[ax] / width) % 2) * scale
+
+
+def create_Xy(x_dim: np.array, y_dim: np.array, poly_degree: int,
+              surface_coeffs: [float],
+              texture_fn: Callable | None = None,
+              texture_fn_kwargs: dict | None = None,
+              noise_mean: float | None = None, noise_sigma: float | None = None
+              ) -> (np.array, np.array):
+    """Create Xy values for a test."""
+    surface_coeffs = np.array(surface_coeffs)
+    X = np.array([[y, x] for y in y_dim for x in x_dim])
+
+    # Create surface for X
+    poly = PolynomialFeatures(degree=poly_degree)
+    poly_x = poly.fit_transform(X)
+
+    surface_y = poly_x @ surface_coeffs
+
+    y = surface_y
+    if texture_fn is not None and texture_fn_kwargs is not None:
+        texture_y = np.array([texture_fn(x, **texture_fn_kwargs) for x in X])
+        y += texture_y
+    if noise_sigma is not None and noise_mean is not None:
+        noise_y = noise_sigma * np.random.randn(len(texture_y)) + noise_mean
+        y += noise_y
+
+    return (X, y)
+
+
+def test_surface_plane(plane_surface_coeffs, x_dim, y_dim,
+                       texture_kwargs, noise_mean,
+                       noise_sigma, min_mse_plane, monkeypatch):
+    monkeypatch.setattr(plt, 'show', lambda: None)
+
+    __, gt_surf_y = create_Xy(x_dim, y_dim, 1, plane_surface_coeffs)
+    X, y = create_Xy(x_dim, y_dim, 1, plane_surface_coeffs, step_train,
+                     texture_kwargs, noise_mean=noise_mean,
+                     noise_sigma=noise_sigma)
+    da = surface.convert_Xy_to_xarray(X, y)
+    gt_surf_da = surface.convert_Xy_to_xarray(X, gt_surf_y)
+
+    fitting_methods = [f for f in surface.FittingMethod]
+    poly_degrees = range(2, 4)
+
+    for fitting_method in fitting_methods:
+        for poly_degree in poly_degrees:
+            logger.info(f'Setting up model with fitting: {fitting_method}'
+                        f', degree: {poly_degree}')
+
+            model = surface.create_surface_model(poly_degree, fitting_method)
+            surf_da, score = surface.fit_surface(da, model)
+
+            surface.visualize(da, surf_da)
+            plt.show()
+
+            logger.info(f'Score: {score}')
+
+            # mse = (np.square(surf_da - gt_surf_da)).mean().to_numpy()
+            # logger.info(f'MSE: {mse}')
+
+            # if (fitting_method == surface.FittingMethod.RANSAC and
+            #         poly_degree == 2):
+            #     assert mse < min_mse_plane
+
+
+def test_surface_poly(poly_2nd_order_surface_coeffs, x_dim, y_dim,
+                      texture_kwargs, noise_mean, noise_sigma,
+                      min_mse_poly, monkeypatch):
+    monkeypatch.setattr(plt, 'show', lambda: None)
+
+    __, gt_surf_y = create_Xy(x_dim, y_dim, 2, poly_2nd_order_surface_coeffs)
+    X, y = create_Xy(x_dim, y_dim, 2, poly_2nd_order_surface_coeffs,
+                     step_train, texture_kwargs, noise_mean=noise_mean,
+                     noise_sigma=noise_sigma)
+    da = surface.convert_Xy_to_xarray(X, y)
+    gt_surf_da = surface.convert_Xy_to_xarray(X, gt_surf_y)
+
+    fitting_methods = [f for f in surface.FittingMethod]
+    poly_degrees = range(2, 4)
+
+    for fitting_method in fitting_methods:
+        for poly_degree in poly_degrees:
+            logger.info(f'Setting up model with fitting: {fitting_method}'
+                        f', degree: {poly_degree}')
+
+            model = surface.create_surface_model(poly_degree, fitting_method)
+            surf_da, score = surface.fit_surface(da, model)
+
+            surface.visualize(da, surf_da)
+            plt.show()
+
+            logger.info(f'Score: {score}')
+
+            # mse = (np.square(surf_da - gt_surf_da)).mean().to_numpy()
+            # logger.info(f'MSE: {mse}')
+
+            # if (fitting_method == surface.FittingMethod.RANSAC and
+            #         poly_degree == 3):
+            #     assert mse < min_mse_poly
