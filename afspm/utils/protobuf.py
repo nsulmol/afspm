c2d367e6494beca1db63bcb7fdbf1285bd7b3e4f
Nick Sullivan <nsulmol@gmail.com>
2025-05-02 14:00:12 -0400

    Revert "[feat] add (unused/broken) check for protobuf"
    This reverts commit 0a7f1df2011b8c80c20916480359e25275a93a35.

--- afspm/utils/protobuf.py
+++ afspm/utils/protobuf.py
@@ -3,7 +3,6 @@
 import math
 
 from google.protobuf.message import Message
-from google.protobuf.descriptor import FieldDescriptor
 
 
 def check_equal(msg1: Message, msg2: Message,
@@ -36,32 +35,4 @@ def check_equal(msg1: Message, msg2: Message,
             return False
         elif not is_float and val1 != val2:  # Value comparison
             return False
-    return True
-
-
-# TODO: Do I need this??? NOte this appears broken :(
-def field_tracks_presence(descriptor: FieldDescriptor) -> bool:
-    """Determine if this field tracks presence or not.
-
-    Depending on the probotuf 'style' (proto2, proto3, editions), different
-    fields may or may not have explicit presence tracking. This method tells
-    us if the field associated with the provided FieldDescriptor does or does
-    not. This is useful to determine if we can call HasField() on the Message
-    to which it belongs.
-
-    NOTE: This method only works for editions 2023, which is what we are
-    currently using. It also assumes field_presence is *not* set to implicit
-    anywhere.
-
-    Args:
-        descriptor: FieldDescriptor of a given field.
-
-    Returns:
-        whether or not the given field tracks exclusive presence.
-    """
-    # TODO: Validate bytes!
-    if descriptor.type in [descriptor.TYPE_ENUM, descriptor.TYPE_BYTES]:
-        return False
-    if descriptor.label == descriptor.LABEL_REPEATED:
-        return False
     return True
