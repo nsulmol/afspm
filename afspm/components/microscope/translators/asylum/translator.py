d7627e368dd075d2ad27d8c5adcd90e858920281
Nick Sullivan <nsulmol@gmail.com>
2025-06-11 11:25:57 -0400

    [feat] update asylum response delay
    It turns out that the asylum is not just slow at spawning, but also slow
    at responding. Thus, we update the default beat delay here to avoid
    heartbeat issues.

--- afspm/components/microscope/translators/asylum/translator.py
+++ afspm/components/microscope/translators/asylum/translator.py
@@ -62,6 +62,7 @@ class AsylumTranslator(ct.ConfigTranslator):
     """
 
     DEFAULT_SPAWN_DELAY_S = 5.0  # Slow startup.
+    DEFAULT_BEAT_DELAY_S = 7.5  # Slow to respond.
 
     IMG_EXT = '.ibw'
     SCAN_PREFIX = 'Image'
