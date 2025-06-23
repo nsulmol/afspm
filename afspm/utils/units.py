[fix] output just magnitude in np unit conversion
The way we are using pint is to encapsulate all the nitty gritty within
utils/units.py. So we don't explicitly want to keep passing around our
objects as (magnitude, units). So we return just the magnitude when we output.

afspm/utils/units.py
@@ -130,4 +130,4 @@ def convert_np(arr: np.ndarray, unit: str | None,
         return arr
 
     arr_w_units = Q_(arr, unit)
-    return arr_w_units.to(desired_unit)
+    return arr_w_units.to(desired_unit).magnitude