f44a66bbc0d22c2155cbe2ba427dd17bfad595d0
Nick Sullivan <nsulmol@gmail.com>
2025-07-16 10:52:00 -0400

    [fix] improper call to swapaxes in prior CL
    I had forgotten to pass the axes to swap :(.

--- afspm/components/microscope/translators/asylum/translator.py
+++ afspm/components/microscope/translators/asylum/translator.py
@@ -377,7 +377,7 @@ def load_scans_from_file(scan_path: str) -> list[scan_pb2] | None:
             # To workaround this, we swap the axes back. The more 'proper' fix
             # would be to simple perform an np.flip(m, 0) rather than these
             # operations.
-            ds = ds.swapaxes()
+            ds = ds.swapaxes(0, 1)
 
             scan = conv.convert_sidpy_to_scan_pb2(ds)
 
