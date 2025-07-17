ad8ff93108a66b9aafe3ce2183944c165258aa2b
Nick Sullivan <nsulmol@gmail.com>
2025-07-17 10:31:15 -0400

    [feat] change keep_scanning logic
    We used to try and grab the latest scan params and redo that. However,
    the latest scan params will be updated by the estimated drift, and we
    know that there will be scans where there is drift (and thus the latest
    scan params are not our original desired ones). Thus, we switch now to
    expliciting a scan region, and just constantly requesting that.

--- samples/keep_scanning/experiment.py
+++ samples/keep_scanning/experiment.py
@@ -7,6 +7,7 @@ from afspm.utils.log import LOGGER_ROOT
 from afspm.components.component import AfspmComponent
 
 from afspm.io.protos.generated import scan_pb2
+from afspm.io.common import create_scan_params_2d
 
 
 logger = logging.getLogger(LOGGER_ROOT + '.samples.keep_scanning.' + __name__)
@@ -16,8 +17,12 @@ logger = logging.getLogger(LOGGER_ROOT + '.samples.keep_scanning.' + __name__)
 class ExperimentData:
     """The data we want to store between calls to get_next_scan_params."""
 
-    scan_id: str  # Envelope for scan id.
-    scan_wait_s: float = 900.0  # 15 mins
+    scan_origin: tuple[float, float]
+    scan_size: tuple[float, float]
+    length_units: str
+    scan_res: tuple[int, int]
+    scan_wait_s: float
+
     scan_sleep_ts: float = None
 
 
@@ -26,7 +31,7 @@ def get_next_scan_params(component: AfspmComponent,
                          ) -> (scan_pb2.ScanParameters2d | None):
     """Choose the next scan method for ScanHandler.
 
-    In this case, we are just re-scanning the last scan. The idea is that
+    In this case, we are just re-scanning the provided scan. The idea is that
     we keep scanning the same region over a period of time.
 
     Returns:
@@ -40,19 +45,10 @@ def get_next_scan_params(component: AfspmComponent,
                          'Returning None.')
             return None
 
-    envelopes = [env for env in list(component.subscriber.cache.keys())
-                 if exp_data.scan_id in env]
-    if len(envelopes) == 0:
-        logger.debug('No protos containing provided scan_id '
-                     f'{exp_data.scan_id} received.')
-        return None
-
-    env = envelopes[0]  # Grab first envelope that matches our scan_id
-    if len(component.subscriber.cache[env]) == 0:
-        logger.debug(f'No scan has been received with env {env}!')
-        return None
-
     logger.debug('Sending out old scan params to keep scanning.')
-    scan_params = component.subscriber.cache[env][-1].params
+    scan_params = create_scan_params_2d(exp_data.scan_origin,
+                                        exp_data.scan_size,
+                                        exp_data.length_units,
+                                        data_shape=exp_data.scan_res)
     exp_data.scan_sleep_ts = time.time()
     return scan_params
